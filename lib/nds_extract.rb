bb39e1ef7a6fd48aae1b0eeba2aaba030dfe1ef8
Michael Quigley <mikequigley@quigleypublishing.com>
2020-01-18 22:40:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,6 +31,7 @@ while director_index < directors_database.length do
     total += directors_database[director_index][:movies][movie_index][:worldwide_gross]
     movie_index += 1
   end
+  end
 result[[directors_database][director_index][:name]] = 50 #directors_database[director_index][:movies][movie_index][:worldwide_gross]
 movie_index += 1
 director_index += 1
